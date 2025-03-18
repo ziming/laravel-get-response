6d1f1bdbd0faab9879608b31dc2fa8217f235b78
ziming <ziming@users.noreply.github.com>
2025-03-18 15:42:44 +0800

    initial connector

--- src/LaravelGetResponse.php
+++ src/LaravelGetResponse.php
@@ -4,4 +4,25 @@ declare(strict_types=1);
 
 namespace Ziming\LaravelGetResponse;
 
-class LaravelGetResponse {}
+use Saloon\Contracts\Authenticator;
+use Saloon\Http\Auth\HeaderAuthenticator;
+use Saloon\Http\Auth\TokenAuthenticator;
+use Saloon\Http\Connector;
+
+/*
+ * Add Oauth authenticator in the future
+ */
+class LaravelGetResponse extends Connector
+{
+    public function __construct(protected readonly string $token){}
+
+    protected function defaultAuth(): HeaderAuthenticator
+    {
+        return new HeaderAuthenticator($this->token, 'X-AUTH-TOKEN');
+    }
+
+    public function resolveBaseUrl(): string
+    {
+        return 'https://api.getresponse.com/v3/';
+    }
+}
