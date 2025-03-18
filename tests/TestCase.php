07a70bdc77eea5a142c42457bafac44afa38dec0
ziming <679513+ziming@users.noreply.github.com>
2025-03-18 07:22:46 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Ziming\LaravelGetResponse\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Ziming\LaravelGetResponse\LaravelGetResponseServiceProvider;
 
