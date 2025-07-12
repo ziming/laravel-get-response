406bd162785fc21f00f34a640dc2dcd1311df960
ziming <ziming@users.noreply.github.com>
2025-07-12 20:17:53 +0800

    add landing page request to Websites folder

---
+++ src/Requests/Websites/GetLandingPageByIdRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ziming\LaravelGetResponse\Requests\Websites;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+
+class GetLandingPageByIdRequest extends Request
+{
+    protected Method $method = Method::GET;
+
+    public function __construct(protected readonly string $landingPageId, protected readonly array $fields = [])
+    {
+    }
+
+    protected function defaultQuery(): array
+    {
+        return [
+            'fields' => implode(',', $this->fields),
+        ];
+    }
+
+    public function resolveEndpoint(): string
+    {
+        return '/landing-pages/' . $this->landingPageId;
+    }
+}
