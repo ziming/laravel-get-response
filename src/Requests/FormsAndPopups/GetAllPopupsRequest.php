6a09564432f90dd1e40eebe6e5429cb3b445cb01
ziming <ziming@users.noreply.github.com>
2025-07-12 16:45:59 +0800

    initial form and popups requests

---
+++ src/Requests/FormsAndPopups/GetAllPopupsRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ziming\LaravelGetResponse\Requests\FormsAndPopups;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+
+class GetAllPopupsRequest extends Request
+{
+    protected Method $method = Method::GET;
+
+    public function __construct(
+        protected readonly array $queryParameters = [],
+        protected readonly array $statsParameters = [],
+        protected readonly array $sortParameters = [],
+        protected readonly array $fields = [],
+        protected readonly int $page = 1,
+        protected readonly int $perPage = 100,
+        protected readonly bool $withStatistics = false,
+    ) {
+    }
+
+    protected function defaultQuery(): array
+    {
+        return array_filter([
+            'query' => $this->queryParameters,
+            'stats' => $this->statsParameters,
+            'sort' => $this->sortParameters,
+            'fields' => implode(',', $this->fields),
+            'page' => $this->page,
+            'perPage' => $this->perPage,
+        ]);
+    }
+
+    public function resolveEndpoint(): string
+    {
+        return '/popups';
+    }
+}
---
+++ src/Requests/FormsAndPopups/GetPopupByIdRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ziming\LaravelGetResponse\Requests\FormsAndPopups;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+
+class GetPopupByIdRequest extends Request
+{
+    protected Method $method = Method::GET;
+
+    public function __construct(
+        protected readonly string $popupId,
+        protected readonly array $fields = [],
+    ) {
+    }
+
+    protected function defaultQuery(): array
+    {
+        return array_filter([
+            'fields' => implode(',', $this->fields),
+        ]);
+    }
+
+    public function resolveEndpoint(): string
+    {
+        return '/popups/' . $this->popupId;
+    }
+}
