83e8c83935e6f8f92fd65ce41c2fbdaed6a6db0d
ziming <ziming@users.noreply.github.com>
2025-07-12 16:06:02 +0800

    Add initial websites requests

---
+++ src/Requests/Websites/GetAllWebsitesRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ziming\LaravelGetResponse\Requests\Websites;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+
+class GetAllWebsitesRequest extends Request
+{
+    protected Method $method = Method::GET;
+
+    public function __construct(
+        protected readonly array $queryParameters = [],
+        protected readonly array $sortParameters = [],
+        protected readonly array $fields = [],
+        protected readonly int $page = 1,
+        protected readonly int $perPage = 100,
+    ) {
+    }
+
+    protected function defaultQuery(): array
+    {
+        return array_filter([
+            'query' => $this->queryParameters,
+            'sort' => $this->sortParameters,
+            'fields' => implode(',', $this->fields),
+            'page' => $this->page,
+            'perPage' => $this->perPage,
+        ]);
+    }
+
+    public function resolveEndpoint(): string
+    {
+        return '/websites';
+    }
+}
---
+++ src/Requests/Websites/GetWebsiteByIdRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ziming\LaravelGetResponse\Requests\Websites;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+
+class GetWebsiteByIdRequest extends Request
+{
+    protected Method $method = Method::GET;
+
+    public function __construct(protected readonly string $websiteId, protected readonly array $fields = [])
+    {
+    }
+
+    protected function defaultQuery(): array
+    {
+        return [
+            'fields' => implode(',', $this->fields),
+        ];
+    }
+
+    public function resolveEndpoint(): string
+    {
+        return '/websites/' . $this->websiteId;
+    }
+}
