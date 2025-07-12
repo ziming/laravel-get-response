65081416db0db72006e3423272a3083845cde9c0
ziming <ziming@users.noreply.github.com>
2025-07-12 15:58:48 +0800

    initial Webinar Requests

---
+++ src/Requests/Webinars/GetWebinarByIdRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ziming\LaravelGetResponse\Requests\Webinars;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+
+class GetWebinarByIdRequest extends Request
+{
+    protected Method $method = Method::GET;
+
+    public function __construct(protected readonly string $webinarId, protected readonly array $fields = [])
+    {
+    }
+
+    protected function defaultQuery(): array
+    {
+        return [
+            'fields' => implode(',', $this->fields),
+        ];
+    }
+
+    public function resolveEndpoint(): string
+    {
+        return '/webinars/' . $this->webinarId;
+    }
+}
